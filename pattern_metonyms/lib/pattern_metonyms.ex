46e12d8e3f9d28232ca44bf61e910c0c9f1e5a60
nathanael <nathanael.pineau@gmail.com>
2020-11-29 14:54:11 +0100

    update on patten metonyms

--- pattern_metonyms/lib/pattern_metonyms.ex
+++ pattern_metonyms/lib/pattern_metonyms.ex
@@ -4,15 +4,176 @@ defmodule PatternMetonyms do
   """
 
   @doc """
-  Hello world.
+  implicit bidirectional
+  target : pattern just2(a, b) = just({a, b})
+  currently work as is for that kind of complexity
 
-  ## Examples
+  unidirectional
+  target : pattern head(x) <- [x | _]
+  but doesn't work as is
+  "pattern(head(x) <- [x | _])"
 
-      iex> PatternMetonyms.hello()
-      :world
-
+  explicit bidirectional
+  target : pattern polar(r, a) <- (pointPolar -> {r, a}) when polar(r, a) = polarPoint(r, a)
+  but doesn't work as is
+  "pattern (polar(r, a) <- (pointPolar -> {r, a})) when polar(r, a) = polarPoint(r, a) "
   """
-  def hello do
-    :world
+  # implicit bidirectional
+  # lhs = {:just2, [], [{:a, [], Elixir}, {:b, [], Elixir}]}  # just2(a, b)
+  # pat = {:just, [], [{{:a, [], Elixir}, {:b, [], Elixir}}]} # just({a, b})
+  defmacro pattern(_syn = {:=, _, [lhs, pat]}) do
+    # _ = IO.inspect(syn, label: "implicit bidirectional(syn)")
+    # _ = IO.inspect(lhs, label: "implicit bidirectional(lhs)")
+    # _ = IO.inspect(pat, label: "implicit bidirectional(pat)")
+    {name, meta, args} = lhs
+    quote do
+      defmacro unquote({name, meta, [:viewing | args]}) do
+        ast_args = unquote(Macro.escape(args))
+        args = unquote(args)
+        relate = fn {{name, _, con}, substitute} -> {{name, con}, substitute} end
+        args_relation = Map.new(Enum.zip(ast_args, args), relate)
+
+        ast_pat = unquote(Macro.escape(pat))
+
+        Macro.prewalk(ast_pat, fn x ->
+          case {ast_var?(x), x} do
+            {false, x} -> x
+            {true, {name, _, con}} ->
+              case Map.fetch(args_relation, {name, con}) do
+                :error -> x
+                {:ok, substitute} -> substitute
+              end
+          end
+        end)
+        #|> case do x -> _ = IO.puts("#{unquote(name)} [implicit bidirectional]:\n#{Macro.to_string(x)}") ; x end
+      end
+
+      defmacro unquote(lhs) do
+        ast_args = unquote(Macro.escape(args))
+        args = unquote(args)
+        relate = fn {{name, _, con}, substitute} -> {{name, con}, substitute} end
+        args_relation = Map.new(Enum.zip(ast_args, args), relate)
+
+        ast_pat = unquote(Macro.escape(pat))
+
+        Macro.prewalk(ast_pat, fn x ->
+          case {ast_var?(x), x} do
+            {false, x} -> x
+            {true, {name, _, con}} ->
+              case Map.fetch(args_relation, {name, con}) do
+                :error -> x
+                {:ok, substitute} -> substitute
+              end
+          end
+        end)
+        #|> case do x -> _ = IO.puts("#{unquote(name)} [implicit bidirectional]:\n#{Macro.to_string(x)}") ; x end
+      end
+    end
+    #|> case do x -> _ = IO.puts("pattern [implicit bidirectional]:\n#{Macro.to_string(x)}") ; x end
   end
+
+  # unidirectional / with view
+  defmacro pattern(syn = {:<-, _, [lhs, view = [{:->, _, [[fun], pat]}]]}) do
+    _ = IO.inspect(syn,  label: "implicit bidirectional(syn)")
+    _ = IO.inspect(lhs,  label: "unidirectional(lhs)")
+    _ = IO.inspect(view, label: "unidirectional(view)")
+    _ = IO.inspect(fun,  label: "unidirectional(fun)")
+    _ = IO.inspect(pat,  label: "unidirectional(pat)")
+  end
+
+  # unidirectional
+  # lhs = {:head, [], [{:x, [], Elixir}]}                  # head(x)
+  # pat = [{:|, [], [{:x, [], Elixir}, {:_, [], Elixir}]}] # [x | _]
+  defmacro pattern({:<-, _, [lhs, pat]}) do
+    {name, meta, args} = lhs
+    quote do
+      defmacro unquote({name, meta, [:viewing | args]}) do
+        ast_args = unquote(Macro.escape(args))
+        args = unquote(args)
+        relate = fn {{name, _, con}, substitute} -> {{name, con}, substitute} end
+        args_relation = Map.new(Enum.zip(ast_args, args), relate)
+
+        ast_pat = unquote(Macro.escape(pat))
+
+        Macro.prewalk(ast_pat, fn x ->
+          case {ast_var?(x), x} do
+            {false, x} -> x
+            {true, {name, _, con}} ->
+              case Map.fetch(args_relation, {name, con}) do
+                :error -> x
+                {:ok, substitute} -> substitute
+              end
+          end
+        end)
+        #|> case do x -> _ = IO.puts("#{unquote(name)} [implicit bidirectional]:\n#{Macro.to_string(x)}") ; x end
+      end
+
+      defmacro unquote(lhs) do
+        ast_args = unquote(Macro.escape(args))
+        args = unquote(args)
+        relate = fn {{name, _, con}, substitute} -> {{name, con}, substitute} end
+        args_relation = Map.new(Enum.zip(ast_args, args), relate)
+
+        ast_pat = unquote(Macro.escape(pat))
+
+        Macro.prewalk(ast_pat, fn x ->
+          case {ast_var?(x), x} do
+            {false, x} -> x
+            {true, {name, _, con}} ->
+              case Map.fetch(args_relation, {name, con}) do
+                :error -> x
+                {:ok, substitute} -> substitute
+              end
+          end
+        end)
+        #|> case do x -> _ = IO.puts("#{unquote(name)} [implicit bidirectional]:\n#{Macro.to_string(x)}") ; x end
+      end
+    end
+    #|> case do x -> _ = IO.puts("pattern [implicit bidirectional]:\n#{Macro.to_string(x)}") ; x end
+  end
+
+  # explicit bidirectional / with view
+  # lhs = {:polar, [], [{:r, [], Elixir}, {:a, [], Elixir}]}       # polar(r, a)
+  # fun = {:pointPolar, [], Elixir}                                # pointPolar
+  # pat = {{:r, [], Elixir}, {:a, [], Elixir}}                     # {r, a}
+  # lhs2 = {:polar, [], [{:r, [], Elixir}, {:a, [], Elixir}]}      # polar(r, a)
+  # expr = {:polarPoint, [], [{:r, [], Elixir}, {:a, [], Elixir}]} # polarPoint(r, a)
+  defmacro pattern(where = {:when, _, [link = {:<-, _, [lhs, view = [{:->, _, [[fun], pat]}]]}, builder = {:=, _, [lhs2, expr]}]}) do
+    _ = IO.inspect(where,   label: "explicit bidirectional(where)")
+    _ = IO.inspect(link,    label: "explicit bidirectional(link)")
+    _ = IO.inspect(lhs,     label: "explicit bidirectional(lhs)")
+    _ = IO.inspect(view,    label: "explicit bidirectional(view)")
+    _ = IO.inspect(fun,     label: "explicit bidirectional(fun)")
+    _ = IO.inspect(pat,     label: "explicit bidirectional(pat)")
+    _ = IO.inspect(builder, label: "explicit bidirectional(builder)")
+    _ = IO.inspect(lhs2,    label: "explicit bidirectional(lhs2)")
+    _ = IO.inspect(expr,    label: "explicit bidirectional(expr)")
+  end
+
+  # explicit bidirectional
+  defmacro pattern(where = {:when, _, [link = {:<-, _, [lhs, pat]}, builder = {:=, _, [lhs2, expr]}]}) do
+    _ = IO.inspect(where,   label: "explicit bidirectional(where)")
+    _ = IO.inspect(link,    label: "explicit bidirectional(link)")
+    _ = IO.inspect(lhs,     label: "explicit bidirectional(lhs)")
+    _ = IO.inspect(pat,     label: "explicit bidirectional(pat)")
+    _ = IO.inspect(builder, label: "explicit bidirectional(builder)")
+    _ = IO.inspect(lhs2,    label: "explicit bidirectional(lhs2)")
+    _ = IO.inspect(expr,    label: "explicit bidirectional(expr)")
+  end
+
+  defmacro pattern(ast) do
+    _ = IO.inspect(ast, label: "unrecognizable pattern")
+    raise("pattern not recognized")
+  end
+
+  # TODO : when using
+  # view x do
+  #   <pattern>(...) -> ...
+  # end
+  # call the pattern with an added argument telling the macro that it is in a match
+
+  # Utils
+
+  def ast_var?({name, meta, con}) when is_atom(name) and is_list(meta) and is_atom(con), do: true
+  def ast_var?(_), do: false
 end
--- pattern_metonyms/test/pattern_metonyms_test.exs
+++ pattern_metonyms/test/pattern_metonyms_test.exs
@@ -2,7 +2,67 @@ defmodule PatternMetonymsTest do
   use ExUnit.Case
   doctest PatternMetonyms
 
-  test "greets the world" do
-    assert PatternMetonyms.hello() == :world
+  test "maybe pair" do
+    defmodule TestM2 do
+      import PatternMetonyms
+
+      pattern just2(a, b) = {:Just, {a, b}}
+
+      def f(just2(x, y)), do: x + y
+      def foo, do: f(just2(3, 2))
+    end
+
+    assert TestM2.foo == 5
+  end
+
+  test "maybe triplet" do
+    defmodule TestM3 do
+      import PatternMetonyms
+
+      pattern just3(a, b, c) =
+        {:Just, {a, b, c}}
+
+      def g(just3(x, y, _)), do: x + y
+      def bar, do: g(just3(3, 2, 1))
+    end
+
+    assert TestM3.bar == 5
+  end
+
+  test "maybe singleton" do
+    defmodule TestM1 do
+      import PatternMetonyms
+
+      pattern just1(a) = {:Just, a}
+
+      def h(just1(x)), do: -x
+      def baz, do: h(just1(3))
+    end
+
+    assert TestM1.baz == -3
+  end
+
+  test "list head" do
+    defmodule TestL1 do
+      import PatternMetonyms
+
+      pattern head(x) <- [x | _]
+
+      def f(head(x)), do: x
+    end
+
+    assert TestL1.f([1, 2, 3]) == 1
+  end
+
+  test "raise list head" do
+    assert_raise CompileError, fn ->
+      defmodule TestL2 do
+        import PatternMetonyms
+
+        pattern head(x) <- [x | _]
+
+        def g, do: head(4)
+      end
+    end
   end
 end
