18f76a95ca6f5525d43fc11d33382f1193a77348
Shakadak <Shakadak@users.noreply.github.com>
2024-12-01 14:23:13 +0100

    refacto + tests

--- nbe/lib/nbe.ex
+++ nbe/lib/nbe.ex
@@ -1,18 +1,92 @@
 defmodule Nbe do
-  @moduledoc """
-  Documentation for `Nbe`.
-  """
+  import Clos
 
-  @doc """
-  Hello world.
+  @type environment :: Keyword.t
+  @type symbol :: atom
+  @type value :: any
+  @type expression :: any
+  @type void :: {}
+  @type clos :: Clos.t
+  @type neutral :: any
 
-  ## Examples
+  @spec assv(any, list(tuple)) :: tuple | false
+  def assv(k, lst) do
+    Keyword.fetch(lst, k)
+    |> case do
+      {:ok, v} -> {k, v}
+      :error -> false
+    end
+  end
 
-      iex> Nbe.hello()
-      :world
+  @spec cdr(tuple) :: any
+  def cdr({_, y}), do: y
 
-  """
-  def hello do
-    :world
+  @spec car(tuple) :: any
+  def car({x, _}), do: x
+
+  def cons(a, d) when is_list(d), do: [a | d]
+  def cons(a, d), do: {a, d}
+
+  @spec displayln(any) :: void
+  def displayln(v), do: (IO.inspect(v); {})
+
+  @spec memv(any, list | any) :: false | list
+  def memv(x, [x | _] = lst), do: lst
+  def memv(_, []), do: false
+  def memv(x, [_ | rest]), do: memv(x, rest)
+
+  @spec extend(environment, symbol, value) :: environment
+  def extend(p, x, v), do:
+    cons(cons(x, v), p)
+
+  @spec val(environment, expression) :: value
+  def val(p, e) do
+    case e do
+      [:lam, [x], b] -> clos(p, x, b)
+      x when is_atom(x) ->
+        xv = assv(x, p)
+        if xv do
+          cdr(xv)
+        else
+          raise "Unknown variable #{inspect(x)}"
+        end
+
+      [rator, rand] ->
+        do_ap(val(p, rator), val(p, rand))
+    end
+  end
+
+  @spec do_ap(clos, value) :: value
+  def do_ap(clos, arg) do
+    case clos do
+      %Clos{env: p, var: x, body: b} ->
+        val(extend(p, x, arg), b)
+    end
+  end
+
+  @spec run_program(environment, list(expression)) :: void
+  def run_program(p, exprs) do
+    case exprs do
+      [] -> {}
+      [[:define, x, e] | rest] ->
+        v = val(p, e)
+        run_program(extend(p, x, v), rest)
+
+      [e | rest] ->
+        {} = displayln(val(p, e))
+        run_program(p, rest)
+    end
+  end
+
+  @spec add_star(symbol) :: symbol
+  def add_star(x), do: :"#{x}*"
+
+  @spec freshen(list(symbol), symbol) :: symbol
+  def freshen(used, x) do
+    if memv(x, used) do
+      freshen(used, add_star(x))
+    else
+      x
+    end
   end
 end
--- nbe/test/nbe_test.exs
+++ nbe/test/nbe_test.exs
@@ -2,7 +2,73 @@ defmodule NbeTest do
   use ExUnit.Case
   doctest Nbe
 
-  test "greets the world" do
-    assert Nbe.hello() == :world
+  import ExUnit.CaptureIO
+
+  import Nbe
+
+  test "assv" do
+    {:x, "apples"} =
+      assv(:x, [{:y, "peaches"}, {:x, "apples"}])
+
+    false =
+      assv(:x,[{:y, "peaches"}, {:z, "apples"}])
+  end
+
+  test "val" do
+    import Clos
+
+    assert clos([], :x, [:lam, [:y], :y]) =
+      val([], [:lam, [:x], [:lam, [:y], :y]])
+
+    assert clos([], :x, :x) =
+      val([], [[:lam, [:x], :x], [:lam, [:x], :x]])
+
+    assert_raise RuntimeError, "Unknown variable :x", fn -> val([], :x) end
+  end
+
+  test "run-program" do
+    import Clos
+
+    cmp = inspect(
+      clos([{:id, clos([], :x, :x)}], :y, [:lam, [:z], [:z, :y]]),
+      pretty: true
+    ) <> "\n"
+    assert ^cmp = capture_io fn ->
+      run_program([], [[:define, :id, [:lam, [:x], :x]],
+        [:id, [:lam, [:y], [:lam, [:z], [:z, :y]]]]])
+    end
+
+    cmp = inspect(
+      clos(
+        [
+          {:n, clos([{:n, clos([], :f, [:lam, [:x], :x])},{:z, clos([], :f, [:lam, [:x], :x])}], :f, [:lam, [:x], [:f, [[:n, :f], :x]]]) },
+          {:z, clos([], :f, [:lam, [:x], :x])},
+        ],
+        :f,
+        [:lam, [:x], [:f, [[:n, :f], :x]]]
+      ),
+      pretty: true
+    ) <> "\n"
+
+    assert ^cmp = capture_io fn ->
+      run_program([], [
+        [:define, :z,
+          [:lam, [:f],
+            [:lam, [:x], :x]]],
+        [:define, :s,
+          [:lam, [:n],
+            [:lam, [:f],
+              [:lam, [:x],
+                [:f, [[:n, :f], :x]]]]]],
+        [:s, [:s, :z]],
+      ])
+    end
+  end
+
+  test "freshen" do
+    assert :x = freshen([], :x)
+    assert :"x**" = freshen([:x, :"x*"], :x)
+    assert :"y*" = freshen([:x, :y, :z], :y)
+
   end
 end
