a87020dc42f4d70862c9b1e14735ff072a3e3001
Nathanael <Shakadak@users.noreply.github.com>
2025-01-27 12:15:48 +0100

    named lifted fn

--- lambda_lifting/lib/lambda_lifting.ex
+++ lambda_lifting/lib/lambda_lifting.ex
@@ -9,6 +9,7 @@ defmodule LambdaLifting do
 
       import unquote(__MODULE__), only: [
         lfn: 1,
+        lfn: 2,
       ]
     end
   end
@@ -20,26 +21,36 @@ defmodule LambdaLifting do
     body
   end
 
-  defmacro lfn(do: ast) do
+  defmacro lfn(opts \\ [], do: ast) do
+    do_lfn(opts, ast, __CALLER__)
+  end
+
+  def do_lfn(opts, ast, caller) do
     ast = normalize_body(ast)
 
-    instances = Module.get_attribute(__CALLER__.module, :"$$lfn-instances", %{})
-    {n, instances} = Map.get_and_update(instances, __CALLER__.function, fn 
+    instances = Module.get_attribute(caller.module, :"$$lfn-instances", %{})
+    {n, instances} = Map.get_and_update(instances, caller.function, fn 
       nil -> {0, 0 + 1}
       n   -> {n, n + 1}
     end)
-    :ok = Module.put_attribute(__CALLER__.module, :"$$lfn-instances", instances)
+    :ok = Module.put_attribute(caller.module, :"$$lfn-instances", instances)
 
-    {cf_name, cf_arity} = __CALLER__.function
-    fun_name = :"_lfn-#{cf_name}-#{cf_arity}-#{n}"
+    fun_name = case Keyword.fetch(opts, :name) do
+      {:ok, name} -> name
+      :error ->
+        {cf_name, cf_arity} = caller.function
+        :"_lfn-#{cf_name}-#{cf_arity}-#{n}"
+    end
 
-    versioned_vars = __CALLER__.versioned_vars
+    versioned_vars = caller.versioned_vars
     versioned_args =
       Enum.sort(Map.keys(versioned_vars))
       |> Enum.map(fn {name, context} -> Macro.var(name, context) end)
 
     asts =
       Enum.map(ast, fn {:->, _, [pattern, expression]} ->
+        # Find the variables that are defined in the pattern, they will shadow
+        # the variable accessible from the outside scope.
         {_, shadowing_vars} = Macro.prewalk(pattern, MapSet.new(), fn
           {name, _, context} = ast, shadowing_vars when is_atom(name) and is_atom(context) ->
             shadowing_vars = MapSet.put(shadowing_vars, {name, context})
@@ -48,6 +59,8 @@ defmodule LambdaLifting do
           ast, acc -> {ast, acc}
         end)
 
+        # Replace shadowed variables with `_` to avoid matching conflicts, eg.
+        # `(x, x, y)` becomes `(x, _, y)`
         versioned_args = Enum.map(versioned_args, fn {name, meta, context} ->
           case MapSet.member?(shadowing_vars, {name, context}) do
             true  -> {:_, meta, context}
@@ -63,8 +76,8 @@ defmodule LambdaLifting do
       end)
     # |> tap(&IO.puts(Macro.to_string(&1)))
 
-    defs = Module.get_attribute(__CALLER__.module, :"$$lfn-defs", [])
-    :ok = Module.put_attribute(__CALLER__.module, :"$$lfn-defs", defs ++ asts)
+    defs = Module.get_attribute(caller.module, :"$$lfn-defs", [])
+    :ok = Module.put_attribute(caller.module, :"$$lfn-defs", defs ++ asts)
 
     [{:->, _, [pat, _expr]}] = ast
     argsn = length(pat) + length(versioned_args)
--- lambda_lifting/test/lambda_lifting_test.exs
+++ lambda_lifting/test/lambda_lifting_test.exs
@@ -12,8 +12,16 @@ defmodule LambdaLiftingTest do
         y = 1
         lfn do x -> x + y end
       end
+
+      def mk2(a) do
+        lfn name: :lifted do b -> a * b end
+      end
     end
+
     {fun, env} = LL1.mk()
-    assert apply(fun, [1] ++ env) |> IO.inspect() == 2
+    assert apply(fun, [1 | env]) == 2
+
+    {fun, env} = LL1.mk2(2)
+    assert apply(fun, [3 | env]) == 6
   end
 end
