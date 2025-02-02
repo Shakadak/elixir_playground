f1f159296c33a9b8da197a6c565759a65bb80660
Shakadak <Shakadak@users.noreply.github.com>
2025-02-02 20:04:04 +0100

    queue : initial commit

---
+++ queue_exploration/lib/queue_exploration.ex
@@ -0,0 +1,18 @@
+defmodule QueueExploration do
+  @moduledoc """
+  Documentation for `QueueExploration`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QueueExploration.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ queue_exploration/test/queue_exploration_test.exs
@@ -0,0 +1,8 @@
+defmodule QueueExplorationTest do
+  use ExUnit.Case
+  doctest QueueExploration
+
+  test "greets the world" do
+    assert QueueExploration.hello() == :world
+  end
+end
